c694b110bd0190ba4220cde0342e54211b15bad6
Techies98 <100050331+Techies98@users.noreply.github.com>
2022-03-12 12:06:42 +0300

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,11 @@
+function Block(Letter, rate, used, father, code) {
+this.letter = letter;
+this.rate = rate;
+this.used = used;
+this.father = father;
+this.code = code;
+}
+
+function FindCode(block) {
+
+if (tree[block.father].code != **) {
